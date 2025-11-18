1d93e21665c1947da955fd1ecefdba9734cf06ce
Ashraf Zallouz <ashrafzallouz@docform.ai>
2025-11-18 10:06:28 +0100

    feat: improve display clients list

--- src/components/ui/dashboard/dashboardClients.tsx
+++ src/components/ui/dashboard/dashboardClients.tsx
@@ -118,11 +118,12 @@ export const DashboardClientsComponent = () => {
                 <th className="px-6 py-4 text-left text-sm font-semibold text-slate-700">
                   Contact
                 </th>
-                <th className="px-6 py-4 text-left text-sm font-semibold text-slate-700">
-                  Phone
-                </th>
+
                 <th className="px-6 py-4 text-left text-sm font-semibold text-slate-700">
                   Adresse
+                </th>
+                <th className="px-6 py-4 text-left text-sm font-semibold text-slate-700">
+                  Type
                 </th>
                 <th className="px-6 py-4 text-right text-sm font-semibold text-slate-700">
                   Actions
@@ -149,13 +150,13 @@ export const DashboardClientsComponent = () => {
                       <p className="text-slate-500">{client.phone}</p>
                     </div>
                   </td>
-                  <td className="px-6 py-4 text-slate-600 text-sm">
-                    {client.phone}
-                  </td>
+
                   <td className="px-6 py-4">
                     <span className=" text-slate-600">{client.address}</span>
                   </td>
-
+                  <td className="px-6 py-4 font-bold text-slate-600 text-sm">
+                    {client.type}
+                  </td>
                   <td className="px-6 py-4">
                     <div className="flex items-center justify-end gap-2">
                       <button
